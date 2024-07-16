b811bafa914cb24a86419beb2ea59f45f0a11acb
AnaLuizanc <analuizanc13@gmail.com>
2024-07-16 14:28:53 -0300

    add attributes

---
+++ Movie.hpp
@@ -0,0 +1,15 @@
+
+class Movie{
+    string tconst;
+    string titleType;
+    string primaryTitle;
+    string originalTitle;
+    bool isAdult;
+    int startYear;
+    int endYear;
+    int runtimeMinutes;
+    string genres[];
+
+
+
+};
