e29001598437ee9a2713bd786f6a95cdf82dea2e
AnaLuizanc <analuizanc13@gmail.com>
2024-07-16 14:52:04 -0300

    add getters and setters

--- Movie.hpp
+++ Movie.hpp
@@ -1,4 +1,7 @@
 
+#include <string>
+using namespace std;
+
 class Movie{
     string tconst;
     string titleType;
@@ -10,6 +13,80 @@ class Movie{
     int runtimeMinutes;
     string genres[];
 
+    public:
+        void setTconst(string tconst){
+            tconst = tconst;
+        }
+
+        string getTconst(){
+            return tconst;
+        }
+
+        void setTitleType(string titleType){
+            titleType = titleType;
+        }
+
+        string getTitleType(){
+            return titleType;
+        }
+
+        void setPrimaryTitle(string primaryTitle){
+            primaryTitle = primaryTitle;
+        }
+
+        string getPrimaryTitle(){
+            return primaryTitle;
+        }
+
+        void setOriginalTitle(string originalTitle){
+            originalTitle = originalTitle;
+        }
+
+        string getOriginalTitle(){
+            return originalTitle;
+        }
+
+        void setIsAdult(bool isAdult){
+            isAdult = isAdult;
+        }
+
+        bool getIsAdult(){
+            return isAdult;
+        }
+
+        void setStartYear(int startYear){
+            startYear = startYear;
+        }
+
+        int getStartYear(){
+            return startYear;
+        }
+
+        void setEndYear(int endYear){
+            endYear = endYear;
+        }
+
+        int getEndYear(){
+            return endYear;
+        }
+
+        void setRuntimeMinutes(int runtimeMinutes){
+            runtimeMinutes = runtimeMinutes;
+        }
+
+        int getRuntimeMinutes(){
+            return runtimeMinutes;
+        }
+
+        /*void setGenres(string genre, int amount){
+            for (int i = 0; i < amount; i++){
+                genres[i] = genre;
+            }
+        }
+
+        string getGenres(){
+            return 
+        }*/ 
 
 
 };
