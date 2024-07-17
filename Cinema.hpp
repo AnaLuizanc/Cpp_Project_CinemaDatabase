4c0b0649c9c07098efeaf17509659d2d3759c934
AnaLuizanc <analuizanc13@gmail.com>
2024-07-17 15:14:01 -0300

    add Cinema attributes

---
+++ Cinema.hpp
@@ -0,0 +1,12 @@
+#include <string>
+using namespace std;
+
+class Cinema{
+    string cinemaID;
+    string name;
+    int xCoordinate;
+    int yCoordinate;
+    double preco;
+    Movie movies[];
+
+}
