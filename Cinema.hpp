03ea47650286e0028bcad296d6266558403062c2
AnaLuizanc <analuizanc13@gmail.com>
2024-07-17 19:13:33 -0300

    adds getters and setters of the Cinema class attributes

--- Cinema.hpp
+++ Cinema.hpp
@@ -11,8 +11,54 @@ class Cinema{
     double ticketPrice;
     vector<Movie> movies;
 
-    
-        
+    public:
+        void setCinemaID(string cinemaID){
+            this->cinemaID = cinemaID;
+        }
 
+        string getCinemaID(){
+            return cinemaID;
+        }
 
+        void setName(string name){
+            this->name = name;
+        }
+
+        string getName(){
+            return name;
+        }
+
+        void setXCoordinate(int xCoordinate){
+            this->xCoordinate = xCoordinate;
+        }
+
+        int getXCoordinate(){
+            return xCoordinate;
+        }
+
+        void setYCoordinate(int yCoordinate){
+            this->yCoordinate = yCoordinate;
+        }
+
+        int setYCoordinate(){
+            return yCoordinate;
+        }
+
+        void setTicketPrice(int ticketPrice){
+            this->ticketPrice = ticketPrice;
+        }
+
+        double getTicketPrice(){
+            return ticketPrice;
+        }
+
+        void setMovies(string movies[], int amount){
+            for (int i = 0; i < amount; i++){
+                this->movies.push_back(movies[i]);
+            }
+        }
+
+        vector<Movie> getMovies(){
+            return movies;
+        }
 };
